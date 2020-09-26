df96dc30777d86b507b9f644a3f0fc90cb0d1231
Mark Mclarnon <mark.mclarnon@gmail.com>
2020-09-26 13:20:55 -0400

    logging

--- sendfile/backends/nginx.py
+++ sendfile/backends/nginx.py
@@ -1,13 +1,18 @@
 from __future__ import absolute_import
 
+import logging
 from django.http import HttpResponse
 
 from ._internalredirect import _convert_file_to_url
 
+logger = logging.getLogger('django')
 
 def sendfile(request, filename, **kwargs):
     response = HttpResponse()
     url = _convert_file_to_url(filename)
+
+    logger.info( 'converted URL is {}'.format(url) )
+
     response['X-Accel-Redirect'] = url.encode('utf-8')
 
     return response

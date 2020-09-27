61bf2087c626995e47827c6a2038a9cb88dbe535
Mark Mclarnon <mark.mclarnon@gmail.com>
2020-09-26 20:06:13 -0400

    trying to replace HttpResposne

--- sendfile/backends/nginx.py
+++ sendfile/backends/nginx.py
@@ -5,10 +5,354 @@ from django.http import HttpResponse
 
 from ._internalredirect import _convert_file_to_url
 
+
+import datetime
+import json
+import mimetypes
+import os
+import re
+import sys
+import time
+from email.header import Header
+from http.client import responses
+from urllib.parse import quote, urlparse
+
+from django.conf import settings
+from django.core import signals, signing
+from django.core.exceptions import DisallowedRedirect
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http.cookie import SimpleCookie
+from django.utils import timezone
+from django.utils.encoding import iri_to_uri
+from django.utils.http import http_date
+
+_charset_from_content_type_re = re.compile(r';\s*charset=(?P<charset>[^\s;]+)', re.I)
+
 logger = logging.getLogger('django')
 
+
+class BadHeaderError(ValueError):
+    pass
+
+
+class CATOHttpResponseBase:
+    """
+    An HTTP response base class with dictionary-accessed headers.
+
+    This class doesn't handle content. It should not be used directly.
+    Use the HttpResponse and StreamingHttpResponse subclasses instead.
+    """
+
+    status_code = 200
+
+    def __init__(self, content_type=None, status=None, reason=None, charset=None):
+        # _headers is a mapping of the lowercase name to the original case of
+        # the header (required for working with legacy systems) and the header
+        # value. Both the name of the header and its value are ASCII strings.
+        self._headers = {}
+        self._resource_closers = []
+        # This parameter is set by the handler. It's necessary to preserve the
+        # historical behavior of request_finished.
+        self._handler_class = None
+        self.cookies = SimpleCookie()
+        self.closed = False
+        if status is not None:
+            try:
+                self.status_code = int(status)
+            except (ValueError, TypeError):
+                raise TypeError('HTTP status code must be an integer.')
+
+            if not 100 <= self.status_code <= 599:
+                raise ValueError('HTTP status code must be an integer from 100 to 599.')
+        self._reason_phrase = reason
+        self._charset = charset
+        if content_type is None:
+            content_type = 'text/html; charset=%s' % self.charset
+        self['Content-Type'] = content_type
+
+    @property
+    def reason_phrase(self):
+        if self._reason_phrase is not None:
+            return self._reason_phrase
+        # Leave self._reason_phrase unset in order to use the default
+        # reason phrase for status code.
+        return responses.get(self.status_code, 'Unknown Status Code')
+
+    @reason_phrase.setter
+    def reason_phrase(self, value):
+        self._reason_phrase = value
+
+    @property
+    def charset(self):
+        if self._charset is not None:
+            return self._charset
+        content_type = self.get('Content-Type', '')
+        matched = _charset_from_content_type_re.search(content_type)
+        if matched:
+            # Extract the charset and strip its double quotes
+            return matched.group('charset').replace('"', '')
+        return settings.DEFAULT_CHARSET
+
+    @charset.setter
+    def charset(self, value):
+        self._charset = value
+
+    def serialize_headers(self):
+        """HTTP headers as a bytestring."""
+        def to_bytes(val, encoding):
+            return val if isinstance(val, bytes) else val.encode(encoding)
+
+        headers = [
+            (to_bytes(key, 'ascii') + ': ' + to_bytes(value, 'latin-1'))
+            for key, value in self._headers.values()
+        ]
+        return '\r\n'.join(headers)
+
+    __bytes__ = serialize_headers
+
+    @property
+    def _content_type_for_repr(self):
+        return ', "%s"' % self['Content-Type'] if 'Content-Type' in self else ''
+
+    def _convert_to_charset(self, value, charset, mime_encode=False):
+        """
+        Convert headers key/value to ascii/latin-1 native strings.
+
+        `charset` must be 'ascii' or 'latin-1'. If `mime_encode` is True and
+        `value` can't be represented in the given charset, apply MIME-encoding.
+        """
+        if not isinstance(value, (bytes, str)):
+            value = str(value)
+        if ((isinstance(value, bytes) and ('\n' in value or '\r' in value)) or
+                isinstance(value, str) and ('\n' in value or '\r' in value)):
+            raise BadHeaderError("Header values can't contain newlines (got %r)" % value)
+        try:
+            if isinstance(value, str):
+                # Ensure string is valid in given charset
+                value.encode(charset)
+            else:
+                # Convert bytestring using given charset
+                value = value.decode(charset)
+        except UnicodeError as e:
+            if mime_encode:
+                value = Header(value, 'utf-8', maxlinelen=sys.maxsize).encode()
+            else:
+                e.reason += ', HTTP response headers must be in %s format' % charset
+                raise
+        return value
+
+    def __setitem__(self, header, value):
+        header = self._convert_to_charset(header, 'ascii')
+        value = self._convert_to_charset(value, 'latin-1', mime_encode=True)
+        self._headers[header.lower()] = (header, value)
+
+    def __delitem__(self, header):
+        self._headers.pop(header.lower(), False)
+
+    def __getitem__(self, header):
+        return self._headers[header.lower()][1]
+
+    def has_header(self, header):
+        """Case-insensitive check for a header."""
+        return header.lower() in self._headers
+
+    __contains__ = has_header
+
+    def items(self):
+        return self._headers.values()
+
+    def get(self, header, alternate=None):
+        return self._headers.get(header.lower(), (None, alternate))[1]
+
+    def set_cookie(self, key, value='', max_age=None, expires=None, path='/',
+                   domain=None, secure=False, httponly=False, samesite=None):
+        """
+        Set a cookie.
+
+        ``expires`` can be:
+        - a string in the correct format,
+        - a naive ``datetime.datetime`` object in UTC,
+        - an aware ``datetime.datetime`` object in any time zone.
+        If it is a ``datetime.datetime`` object then calculate ``max_age``.
+        """
+        self.cookies[key] = value
+        if expires is not None:
+            if isinstance(expires, datetime.datetime):
+                if timezone.is_aware(expires):
+                    expires = timezone.make_naive(expires, timezone.utc)
+                delta = expires - expires.utcnow()
+                # Add one second so the date matches exactly (a fraction of
+                # time gets lost between converting to a timedelta and
+                # then the date string).
+                delta = delta + datetime.timedelta(seconds=1)
+                # Just set max_age - the max_age logic will set expires.
+                expires = None
+                max_age = max(0, delta.days * 86400 + delta.seconds)
+            else:
+                self.cookies[key]['expires'] = expires
+        else:
+            self.cookies[key]['expires'] = ''
+        if max_age is not None:
+            self.cookies[key]['max-age'] = max_age
+            # IE requires expires, so set it if hasn't been already.
+            if not expires:
+                self.cookies[key]['expires'] = http_date(time.time() + max_age)
+        if path is not None:
+            self.cookies[key]['path'] = path
+        if domain is not None:
+            self.cookies[key]['domain'] = domain
+        if secure:
+            self.cookies[key]['secure'] = True
+        if httponly:
+            self.cookies[key]['httponly'] = True
+        if samesite:
+            if samesite.lower() not in ('lax', 'strict'):
+                raise ValueError('samesite must be "lax" or "strict".')
+            self.cookies[key]['samesite'] = samesite
+
+    def setdefault(self, key, value):
+        """Set a header unless it has already been set."""
+        if key not in self:
+            self[key] = value
+
+    def set_signed_cookie(self, key, value, salt='', **kwargs):
+        value = signing.get_cookie_signer(salt=key + salt).sign(value)
+        return self.set_cookie(key, value, **kwargs)
+
+    def delete_cookie(self, key, path='/', domain=None):
+        # Most browsers ignore the Set-Cookie header if the cookie name starts
+        # with __Host- or __Secure- and the cookie doesn't use the secure flag.
+        secure = key.startswith(('__Secure-', '__Host-'))
+        self.set_cookie(
+            key, max_age=0, path=path, domain=domain, secure=secure,
+            expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        )
+
+    # Common methods used by subclasses
+
+    def make_bytes(self, value):
+        """Turn a value into a bytestring encoded in the output charset."""
+        # Per PEP 3333, this response body must be bytes. To avoid returning
+        # an instance of a subclass, this function returns `bytes(value)`.
+        # This doesn't make a copy when `value` already contains bytes.
+
+        # Handle string types -- we can't rely on force_bytes here because:
+        # - Python attempts str conversion first
+        # - when self._charset != 'utf-8' it re-encodes the content
+        if isinstance(value, (bytes, memoryview)):
+            return bytes(value)
+        if isinstance(value, str):
+            return bytes(value.encode(self.charset))
+        # Handle non-string types.
+        return str(value).encode(self.charset)
+
+    # These methods partially implement the file-like object interface.
+    # See https://docs.python.org/library/io.html#io.IOBase
+
+    # The WSGI server must call this method upon completion of the request.
+    # See http://blog.dscpl.com.au/2012/10/obligations-for-calling-close-on.html
+    def close(self):
+        for closer in self._resource_closers:
+            try:
+                closer()
+            except Exception:
+                pass
+        # Free resources that were still referenced.
+        self._resource_closers.clear()
+        self.closed = True
+        signals.request_finished.send(sender=self._handler_class)
+
+    def write(self, content):
+        raise OSError('This %s instance is not writable' % self.__class__.__name__)
+
+    def flush(self):
+        pass
+
+    def tell(self):
+        raise OSError('This %s instance cannot tell its position' % self.__class__.__name__)
+
+    # These methods partially implement a stream-like object interface.
+    # See https://docs.python.org/library/io.html#io.IOBase
+
+    def readable(self):
+        return False
+
+    def seekable(self):
+        return False
+
+    def writable(self):
+        return False
+
+    def writelines(self, lines):
+        raise OSError('This %s instance is not writable' % self.__class__.__name__)
+
+class CATOHttpResponse(CATOHttpResponseBase):
+    """
+    An HTTP response class with a string as content.
+
+    This content that can be read, appended to, or replaced.
+    """
+
+    streaming = False
+
+    def __init__(self, content='', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Content is a bytestring. See the `content` property methods.
+        self.content = content
+
+    def __repr__(self):
+        return '<%(cls)s status_code=%(status_code)d%(content_type)s>' % {
+            'cls': self.__class__.__name__,
+            'status_code': self.status_code,
+            'content_type': self._content_type_for_repr,
+        }
+
+    def serialize(self):
+        """Full HTTP message, including headers, as a bytestring."""
+        return self.serialize_headers() + '\r\n\r\n' + self.content
+
+    __bytes__ = serialize
+
+    @property
+    def content(self):
+        return ''.join(self._container)
+
+    @content.setter
+    def content(self, value):
+        # Consume iterators upon assignment to allow repeated iteration.
+        if hasattr(value, '__iter__') and not isinstance(value, (bytes, str)):
+            content = ''.join(self.make_bytes(chunk) for chunk in value)
+            if hasattr(value, 'close'):
+                try:
+                    value.close()
+                except Exception:
+                    pass
+        else:
+            content = self.make_bytes(value)
+        # Create a list of properly encoded bytestrings to support write().
+        self._container = [content]
+
+    def __iter__(self):
+        return iter(self._container)
+
+    def write(self, content):
+        self._container.append(self.make_bytes(content))
+
+    def tell(self):
+        return len(self.content)
+
+    def getvalue(self):
+        return self.content
+
+    def writable(self):
+        return True
+
+    def writelines(self, lines):
+        for line in lines:
+            self.write(line)
+
 def sendfile(request, filename, **kwargs):
-    response = HttpResponse()
+    response = CATOHttpResponse()
     url = _convert_file_to_url(filename)
 
     logger.info( 'converted URL is {}'.format(url) )
@@ -17,8 +361,6 @@ def sendfile(request, filename, **kwargs):
 
     logger.debug( response )
     logger.debug( response._headers )
-
-    response._headers['X-Accel-Redirect'] = url
 
     logger.info( "post converted URL is {}".format(response['X-Accel-Redirect']) )
 
